321bf23010e81476182545dae962bb55f885a0a2
Abhishek Brahmbhatt <156850824+brambhattabhishek@users.noreply.github.com>
2025-06-19 17:09:45 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ License:    MIT License
             https://opensource.org/license/mit/
            
             
-GitHub:     https://github.com/GodsScion/Auto_job_applier_linkedIn
+GitHub:     https://github.com/brambhattabhishek/AutoApplyForYou
 
 version:    24.12.29.12.30
 '''
@@ -113,6 +113,6 @@ As an independent developer, I pour my heart and soul into creating tools like t
 Your support, whether through donations big or small or simply spreading the word, means the world to me and helps keep this project alive and thriving.
 
 Gratefully yours 🙏🏻,
-Sai Vignesh Golla
+Abhishek Brahmbhatt
 '''
 ############################################################################################################
